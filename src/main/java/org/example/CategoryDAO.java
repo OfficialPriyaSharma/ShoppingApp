fc8af5e828258d5629b082db56df4f7e977b447c
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-03-09 14:01:41 +0530

    Commit ShoppingApp

--- src/main/java/org/example/CategoryDAO.java
+++ src/main/java/org/example/CategoryDAO.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.util.List;
+
 public interface CategoryDAO {
+
+    public boolean addCategory(Category_Model category_model);
+    public boolean deleteCategory(Category_Model category_model);
+    public boolean updateCategory(Category_Model category_model);
+
+    public List<Category_Model> listCategories();
+    public Category_Model getCategory(int categoryId);
+
 }
